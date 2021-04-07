2e2762aaceb7a866388033b94f1c985a7b69028a
하윤 <gkdbs514@naver.com>
2021-04-07 22:24:37 +0900

    0407

---
+++ app/src/main/java/com/example/study_group/Study.java
@@ -0,0 +1,15 @@
+package com.example.study_group;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Study extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study);
+        //URL = http://data.seoul.go.kr/dataList/195/A/2/datasetView.do
+    }
+}
